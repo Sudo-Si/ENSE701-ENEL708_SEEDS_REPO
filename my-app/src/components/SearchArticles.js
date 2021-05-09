a036093041cfdce66f78b44340136bc81a446084
Frozensapphire <61902105+Frozensapphire@users.noreply.github.com>
2021-05-10 10:22:20 +1200

    added template table

--- my-app/src/components/SearchArticles.js
+++ my-app/src/components/SearchArticles.js
@@ -84,6 +84,27 @@ class SearchArticles extends Component {
                     className="btn btn-outline-warning btn-block mt-4"
                 />
               </form>
+              <table>
+    <thead>
+    <tr>
+        <th>name</th>
+        <th>height</th>
+        <th>place</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>Monte Falco</td>
+        <td>1658</td>
+        <td>Parco Foreste Casentinesi</td>
+    </tr>
+    <tr>
+        <td>Monte Falterona</td>
+        <td>1654</td>
+        <td>Parco Foreste Casentinesi</td>
+    </tr>
+    </tbody>
+</table>
           </div>
           </div>
         </div>
